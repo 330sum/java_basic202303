c3812bdea11a02c2add9c71244a856fb2e652b06
330sum <126743151+330sum@users.noreply.github.com>
2023-04-04 12:43:55 +0900

    상속 공부

---
+++ src/day07/inheri/BMage.java
@@ -0,0 +1,23 @@
+package day07.inheri;
+
+public class BMage extends DPlayer {
+
+    int mana; // 마력게이지 (마법사의 고유속성)
+
+    public BMage(String nickName) {
+        super(nickName);
+        this.mana = 100;
+    }
+
+
+    // 썬더볼트
+    public void thunderBolt() {
+
+    }
+
+    @Override
+    public void showStatus() {
+        super.showStatus();
+        System.out.println("# mana = " + this.mana);
+    }
+}
---
+++ src/day07/inheri/Main.java
@@ -0,0 +1,21 @@
+package day07.inheri;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AWarrior w = new AWarrior("주차왕파킹");
+        BMage m = new BMage("충격왕쇼킹");
+        CHunter h = new CHunter("욕설왕퍼킹");
+
+        w.showStatus();
+        m.showStatus();
+        h.showStatus();
+
+
+
+
+
+
+    }
+}
