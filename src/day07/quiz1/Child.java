d9a8384d86e6cca73a1c4c0971fec18e0ca43d6b
330sum <126743151+330sum@users.noreply.github.com>
2023-04-04 12:44:22 +0900

    퀴즈2

---
+++ src/day07/quiz1/Child.java
@@ -0,0 +1,10 @@
+package day07.quiz1;
+
+public class Child extends Parent{
+
+    public Child(String name) {
+//        super();가 항상 숨어있음 // Parnet() Parnet의 기본생성자 호출
+        super(name);
+        this.name = name;
+    }
+}
---
+++ src/day07/quiz2/Main.java
@@ -0,0 +1,8 @@
+package day07.quiz2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Child c = new Child();
+    }
+}
