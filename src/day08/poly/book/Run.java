9e13aa6f343b0fa9aa775f537af527127b75ec80
330sum <126743151+330sum@users.noreply.github.com>
2023-04-05 16:51:46 +0900

    book CRUD

---
+++ src/day08/poly/book/Run.java
@@ -0,0 +1,20 @@
+package day08.poly.book;
+
+import static day08.poly.book.LibraryView.*;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+//        LibraryView.start();
+        start();
+
+
+//        String keyword = "hello";
+//        String str = "하하호호 hello 안녕~~";
+//
+//        System.out.println(str.contains(keyword)); // true
+
+
+    }
+}
