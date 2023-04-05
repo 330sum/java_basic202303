4a216ff90011162ced565818d2963b9070daff4b
sumin <330sum@naver.com>
2023-04-05 23:56:38 +0900

    다형성 주석 수정

--- src/day08/poly/car/Driver.java
+++ src/day08/poly/car/Driver.java
@@ -15,6 +15,7 @@ public class Driver {
 
 
 
+    // 오버로딩: 메소드명 같은데, 매개변수가 다름
 
 //    public void drive(Mustang m) {
 //        System.out.println("운전을 시작합니다");
@@ -28,7 +29,7 @@ public class Driver {
 
 
 
-
+// 오버로딩 하다가 손꾸락 빠지겠음.(메서드 drive에 매개변수로 싼타페, 그랜저, 벤츠... 위에처럼 계속 적어야함) 이때 필요한 것이 다형성!
     public void dirve(Car c) {
         System.out.println("운전을 시작합니다");
         c.accelerate();
