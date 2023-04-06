fae1f8f65b02fd4247107cca7a2dc2b975c9dfcd
330sum <126743151+330sum@users.noreply.github.com>
2023-04-06 17:38:56 +0900

    퀴즈 쌤이랑

---
+++ src/day09/quiz3/Main.java
@@ -0,0 +1,16 @@
+package day09.quiz3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ArtistView artistView = new ArtistView();
+//        artistView.start();
+
+        ArtistView.start();
+
+
+
+    }
+
+}
---
+++ src/day09/quiz3/Test.java
@@ -0,0 +1,42 @@
+package day09.quiz3;
+
+import day04.array.StringList;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // 가수 객체 생성
+//        Artist artist1 = new Artist();
+
+
+        Artist artist2 = new Artist("동방신기",new StringList("hug"));
+        artist2.info();
+
+
+        // 객체 생성 이후에 노래 추가 하고 싶어
+        // 가수노래 목록 접근해서 노래 추가
+        StringList songList = artist2.getSongList(); // 노래목록 뺴오기
+        songList.push("미로틱"); // 추가하기
+
+        artist2.info();
+
+
+
+
+
+        ArtistRepository ar = new ArtistRepository();
+        ar.addNewArtist("아이유", "분홍신");
+        ar.addNewArtist("동방신기", "롱넘버");
+
+        ArtistRepository.artistList[0].info();
+
+
+        Artist foundArtist = ar.findArtistByName("아이유");
+        System.out.println(foundArtist);
+
+
+
+    }
+
+}
