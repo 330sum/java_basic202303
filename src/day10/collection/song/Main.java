55b0ec0697ea97283a39b6ef28ed41afad93564a
sumin <330sum@naver.com>
2023-04-11 00:46:33 +0900

    주석 수정

--- src/day10/collection/song/Main.java
+++ src/day10/collection/song/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // 세이브파일 자동로드
-//        ArtistRepository.loadFile();
+         //세이브파일 자동로드
+        ArtistRepository.loadFile();
 
 
         ArtistView.start();
